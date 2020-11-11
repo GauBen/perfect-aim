805b21ee42a5c2c9ac02f146fe11d0e1e19fa898
GauBen <benaim.gautier@gmail.com>
2020-11-11 12:51:41 +0100

    style: __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+"""
+Perfect Aim, un jeu développé spécialement pour un hackathon.
+
+Perfect Aim
+===========
+
+Un jeu de stratégie en temps discret développé specialement pour un hacktathon.
+Votre objectif ? Créer un joueur ordinateur meilleur que celui des autres.
+"""
+
 from time import monotonic
 from tkinter import Tk
 
@@ -9,10 +19,14 @@ from players.indianaJones import IndianaJones
 
 
 class Delta:
+    """Temp."""
+
     def __init__(self):
+        """Temp."""
         self.last = monotonic()
 
     def delta(self):
+        """Temp."""
         t = monotonic()
         out = t - self.last
         self.last = t
@@ -30,6 +44,7 @@ if __name__ == "__main__":
     canvas.draw_players(g)
 
     def update():
+        """Temp."""
         dt = delta() * canvas.slider_var.get()
         if dt > 0:
             g.update(dt)

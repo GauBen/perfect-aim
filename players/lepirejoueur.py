23eb843154adaa1524b0162e9461cf6cc49a9f50
GauBen <benaim.gautier@gmail.com>
2020-12-22 14:54:05 +0100

    feat: attack_matrix

--- players/lepirejoueur.py
+++ players/lepirejoueur.py
@@ -1,6 +1,9 @@
 """Stratégie d'exemple : un joueur qui gagne."""
 
+from entities import Fireball
 from fractions import Fraction
+from gamegrid import Tile
+from typing import List, Optional
 from game import Action, Game, Player
 
 ALL_MOVES = (
@@ -10,6 +13,8 @@ ALL_MOVES = (
     Action.MOVE_RIGHT,
 )
 
+FIREBALL_SPEED = Fraction(1, Fireball.INITIAL_SPEED)
+
 
 class LePireJoueur(Player):
     """Stratégie qui gagne."""
@@ -18,6 +23,51 @@ class LePireJoueur(Player):
 
     def play(self, game: Game) -> Action:
         """Choisit la meilleure action possible dans la situation donnée en paramètre."""
+        attack_matrix = self.attack_matrix(game)
+
+        if attack_matrix[self.y][self.x] is not None and self.is_action_valid(
+            attack_matrix[self.y][self.x][0]
+        ):
+            return attack_matrix[self.y][self.x][0]
+
+        interesting_paths = self.list_interesting_paths(game)
+
+        if len(interesting_paths) == 0:
+            return Action.WAIT
+
+        best_t = interesting_paths[0][4]
+        best_moves = interesting_paths[0][3]
+        for (_, _, _, moves, t) in interesting_paths:
+            if t < best_t:
+                best_t = t
+                best_moves = moves
+
+        if self.is_action_valid(best_moves[0]):
+            return best_moves[0]
+
+        return Action.WAIT
+
+    def attack_matrix(self, game: Game) -> List[List[Optional[Action]]]:
+        """Renvoie une liste des directions d'attaque."""
+        matrix = [[None for _ in range(game.size)] for _ in range(game.size)]
+
+        for player in filter(
+            lambda e: e.TILE.is_player() and e.color != self.color,
+            game.entities,
+        ):
+            for a in ALL_MOVES:
+                (x, y) = a.apply((player.x, player.y))
+                t = FIREBALL_SPEED
+                while game.background[y][x] != Tile.WALL:
+                    if matrix[y][x] is None or matrix[y][x][1] > t:
+                        matrix[y][x] = (a.swap().to_attack(), t)
+                    (x, y) = a.apply((x, y))
+                    t += FIREBALL_SPEED
+
+        return matrix
+
+    def list_interesting_paths(self, game: Game) -> list:
+        """Renvoie la liste des chemins se terminant par un item."""
         time_matrix = [[None for _ in range(game.size)] for _ in range(game.size)]
         time_matrix[self.y][self.x] = Fraction(0)
 
@@ -66,17 +116,4 @@ class LePireJoueur(Player):
                         time_matrix[next_y][next_x] = next_t
                         paths.append((next_x, next_y, next_moves, next_t))
 
-        if len(interesting_paths) == 0:
-            return Action.WAIT
-
-        best_t = interesting_paths[0][4]
-        best_moves = interesting_paths[0][3]
-        for (_, _, _, moves, t) in interesting_paths:
-            if t < best_t:
-                best_t = t
-                best_moves = moves
-
-        if self.is_action_valid(best_moves[0]):
-            return best_moves[0]
-
-        return Action.WAIT
+        return interesting_paths
